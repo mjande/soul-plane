d6af64001cd49587a263e5c8eb2cd3c7485b0670
Matthew Anderson <andemat4@oregonstate.edu>
2024-02-28 10:40:18 -0800

    Add cors support

--- server/src/app.ts
+++ server/src/app.ts
@@ -2,18 +2,32 @@
 // Express code adapted from
 // Source URL: https://github.com/osu-cs340-ecampus/nodejs-starter-app
 
-// Date: 2/27.2024
+// Date: 2/27/2024
 // MySQL code adapted from 
 // https://sidorares.github.io/node-mysql2/docs
 
 /* SETUP */
 import 'dotenv/config'
 import express, { Express, Request, Response } from "express";
+import cors from 'cors'
+
 const app: Express = express();
 const port = 55767;
 
+app.use(cors());
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
+
+const allowedOrigins = [
+  'http://flip3.engr.oregonstate.edu',
+  'http://flip2.engr.oregonstate.edu'
+];
+
+const options: cors.CorsOptions = {
+  origin: allowedOrigins
+};
+
+app.use(cors(options))
 
 // Database
 import db from "./database/db-connector"
@@ -21,7 +35,7 @@ import db from "./database/db-connector"
 /* ROUTES */
 app.get("/", async (req: Request, res: Response) => {
   // Define queries
-  const query = 'SELECT * FROM airports;';
+  const query = 'SELECT * FROM Airports;';
 
   // Get results from database
   const [results] = await db.pool.query(query);
@@ -29,6 +43,10 @@ app.get("/", async (req: Request, res: Response) => {
   // Send JSON back to client
   res.send(JSON.stringify(results));
 });
+
+app.get("/test", (req: Request, res: Response) => {
+  res.send("Does this work?")
+})
 
 /* LISTENER */
 app.listen(port, () => {
