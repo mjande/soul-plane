8ab30e9c2703b3cec04a64f6703bf048caee9b49
mattand <mjande@umich.edu>
2024-03-08 16:06:27 -0500

    Add function for fetching flight data in DeleteFlightForm

--- client/src/routes/Flights/DeleteFlightForm.tsx
+++ client/src/routes/Flights/DeleteFlightForm.tsx
@@ -1,4 +1,47 @@
+import Axios from "axios";
+import { useEffect } from "react";
+import { useParams } from "react-router-dom"
+
+interface Flight {
+    flight_id: number,
+    plane_id: number,
+    plane_type: string,
+    depart_aiport_name: string,
+    arrive_airport_name: string,
+    depart_time: Date,
+    arrive_time: Date
+}
+
 export default function DeleteFlightForm() {
+    const { id } = useParams();
+
+    const [flight, setFlight] = useState<Flight>({
+        plane_id: 0,
+        plane_type: '',
+        depart_airport_name: '',
+        arrive_airport_name: '',
+        depart_time: new Date(),
+        arrive_time: new Date()
+    })
+
+    useEffect(() => {
+        async function getFlight() {
+            const response = await Axios.get(`http://${import.meta.env.VITE_HOST_NAME}:55767/flights/${id}`)
+            const data = response.data[0]
+
+            setFlight({
+                flight_id: data.flight_id,
+                plane_type: data.plane_type,
+                depart_airport_name: data.depart_airport_name,
+                arrive_airport_name: data.arrive_airport_name,
+                depart_time: data.depart_time,
+                arrive_time: data.arrive_time
+            })
+        }
+
+        getFlight()
+    }, [id])
+    
     return (
         <div id="delete">
             <form id="deleteFlight" method="post">
