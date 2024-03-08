e1b6000c6b96adcb68ca6f77391e72376cdea43b
mattand <mjande@umich.edu>
2024-03-08 15:55:15 -0500

    Update query in /get/flights/:id

--- server/src/app.ts
+++ server/src/app.ts
@@ -454,7 +454,14 @@ app.get("/flights", async (req: Request, res: Response) => {
 
 app.get("/flights/:id", async (req: Request, res: Response) => {
   try {
-    const selectQuery = `SELECT * FROM Flights WHERE flight_id = ${req.params.id}`
+    const selectQuery = `SELECT flight_id, Flights.plane_id, Plane_types.type_name AS plane_type, depart_airport_id,
+    DepartAirport.airport_name AS depart_airport_name, arrive_airport_id, ArriveAirport.airport_name AS arrive_airport_name,
+    depart_time, arrive_time FROM Flights
+    JOIN Airports AS DepartAirport ON Flights.depart_airport_id = DepartAirport.airport_id
+    JOIN Airports AS ArriveAirport ON Flights.arrive_airport_id = ArriveAirport.airport_id
+    JOIN Planes ON Flights.plane_id = Planes.plane_id
+    JOIN Plane_types ON Planes.plane_type_id = Plane_types.plane_type_id
+    WHERE Flights.flight_id = ${req.params.id};`
 
     const [results] = await db.pool.query(selectQuery)
 
