75555aca933804144cbe95a5d55c56d57ac578e7
mattand <mjande@umich.edu>
2024-03-12 15:11:59 -0400

    Migrate passengerFlights routes to separate file

--- server/src/app.ts
+++ server/src/app.ts
@@ -57,87 +57,8 @@ app.use('/flights', flightsRouter)
 import planesRouter from "./routes/Planes"
 app.use("/planes", planesRouter)
 
-
-/* PassengerFlights */
-
-app.get("/PassengerFlights", async (req: Request, res: Response) => {
-  console.log("first route used")
-  
-  // Define queries
-  const query = 'SELECT * FROM Passengers_flights;';
-
-  // Get results from database
-  const [results] = await db.pool.query(query);
-
-  // Send JSON back to client
-  res.send(JSON.stringify(results));
-});
-
-app.post("/PassengerFlights", async (req: Request, res: Response) => {
-  try {
-    const data = req.body;
-    const flight_id = data.flight_id;
-    const passenger_id = data.passenger_id;
-  
-    const insertQuery = `
-    INSERT INTO Passengers_flights (
-      flight_id, 
-      passenger_id
-    ) VALUES ("${flight_id}", "${passenger_id}");
-  `;
-
-  db.pool.query(
-    insertQuery,
-    [
-      flight_id,
-      passenger_id,
-    ])
-
-    res.json({ success: true, message: 'Passenger added successfully', data });
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ success: false, message: 'Internal Server Error' });
-  }
-})
-
-app.delete("/passengerFlights/:fid/:pid", async (req: Request, res: Response) => {
-  try {
-    const data = req.body;
-    const flight_id = data.flight_id;
-    const passenger_id = data.passenger_id;
-    const deleteQuery = `DELETE FROM Passengers_flights WHERE flight_id = ${flight_id} AND passenger_id = ${passenger_id}`
-
-    db.pool.query(deleteQuery)
-
-    res.json({ success: true, message: 'Plane Type deleted successfully' });
-  } catch(error) {
-    console.error(error);
-    res.status(500).json({ success: false, message: 'Internal Server Error' });
-  }
-})
-
-app.put("/passengerFlights/:fid/:pid", async (req: Request, res: Response) => {
-  try {
-    const fid = req.params.fid;
-    const pid = req.params.pid;
-    const data = req.body;
-    const flight_id = data.flight_id
-    const passenger_id = data.passenger_id
-
-    let updateQuery = `
-      UPDATE Passengers_flights 
-      SET flight_id='${flight_id}', passenger_id ='${passenger_id}' 
-      WHERE flight_id = '${fid}' AND passenger_id = '${pid}'
-    `;
-
-    db.pool.query(updateQuery)
-
-    res.json({ success: true, message: 'Passenger Flights updated successfully', data });
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ success: false, message: 'Internal Server Error' });
-  }
-});
+import passengerFlightsRouter from "./routes/PassengerFlights"
+app.use("/passengerFlights", passengerFlightsRouter)
 
 /* LISTENER */
 app.listen(port, () => {
