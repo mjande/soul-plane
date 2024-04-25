75e9db15a5a3fcb33d6b35d924c9bfc34b2b3351
mattand <mjande@umich.edu>
2024-04-25 19:11:01 -0400

    Try some different sql connection settings

--- server/src/database/db-connector.ts
+++ server/src/database/db-connector.ts
@@ -13,6 +13,8 @@ const pool = mysql.createPool({
         user: process.env.DB_USER,
         password: process.env.DB_PASSWORD,
         database: process.env.DB_NAME,
+        waitForConnections: true,
+        enableKeepAlive: true
     })
 
 export default { pool }
