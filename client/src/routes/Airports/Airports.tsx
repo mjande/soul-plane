bf5799028e4b6b7946546a110ed82708870791f3
mattand <mjande@umich.edu>
2024-10-20 16:24:56 -0400

    Edit api url

--- client/src/routes/Airports/Airports.tsx
+++ client/src/routes/Airports/Airports.tsx
@@ -20,7 +20,7 @@ function Airports() {
     // Source URL: https://axios-http.com/docs/api_intro
     // Date: 3/16/24
     useEffect(() => {
-        Axios.get(`https://${import.meta.env.VITE_BACKEND_HOST}/Airports`).then((response) => {
+        Axios.get(`${import.meta.env.VITE_BACKEND_HOST}/Airports`).then((response) => {
           setAirports(response.data);
         });
       }, [airports]);
